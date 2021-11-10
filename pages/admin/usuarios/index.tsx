d7475f59425427b3310c4bef1e7585ac75f8d2eb
Jonathan Echeverri <87216359+forobjj@users.noreply.github.com>
2021-11-10 01:03:21 -0500

    agregar link al home

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Link from 'next/link';
 
 const index = () => {
     return (
         <div>
-            si te amo
+            <div>Pagina de admin de usuarios</div>
+            <Link href='/'>
+            <a>Ir al Home</a>
+            </Link>
         </div>
     )
 }
