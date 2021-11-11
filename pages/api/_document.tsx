fc93db03138c048356518148a9649afcc8f21365
Jonathan Echeverri <87216359+forobjj@users.noreply.github.com>
2021-11-11 12:49:10 -0500

    agregar titulos en paginas y document para tener iconos

--- pages/api/_document.tsx
+++ pages/api/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script src="https://kit.fontawesome.com/3ce0f76d3b.js" crossorigin='anonymous'></script>
+        <script src="https://kit.fontawesome.com/3ce0f76d3b.js" crossOrigin='anonymous'></script>
         </Head>
         <body style={{backgroundColor: 'red'}}>
           <Main />
